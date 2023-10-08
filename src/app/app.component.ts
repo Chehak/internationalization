aeabbd4c56db9fccc75d71b908b43f0ae385a94a
chehak <gchehak18@gmail.com>
2023-10-08 15:49:06 +0530

    updations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { $localize } from '@angular/localize/init';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'i18n';
+
+  constructor(private titleService: Title) {
+    // this.titleService.setTitle($localize`${this.title}`);
+  }
 }
